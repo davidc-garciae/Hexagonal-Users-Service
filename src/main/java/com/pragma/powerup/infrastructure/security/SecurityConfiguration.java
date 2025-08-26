8fcb3e64bb98bc1bb0c6b61cfa5f2ee779b18dab
davidc-garciae <davidc.garcia@udea.edu.co>
2025-08-26 08:43:03 -0500

    Docs & OpenAPI Update

--- src/main/java/com/pragma/powerup/infrastructure/security/SecurityConfiguration.java
+++ src/main/java/com/pragma/powerup/infrastructure/security/SecurityConfiguration.java
@@ -13,22 +13,24 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableMethodSecurity()
 public class SecurityConfiguration {
 
-  @Bean
-  public SecurityFilterChain securityFilterChain(
-      HttpSecurity http, HeaderAuthenticationFilter headerFilter) throws Exception {
-    http.csrf(AbstractHttpConfigurer::disable)
-        .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-        .authorizeHttpRequests(
-            auth -> auth.requestMatchers(
-                "/actuator/**",
-                "/swagger-ui/**",
-                "/v3/api-docs/**",
-                "/api/v1/auth/login",
-                "/api/v1/users/customer")
-                .permitAll()
-                .anyRequest()
-                .authenticated())
-        .addFilterBefore(headerFilter, UsernamePasswordAuthenticationFilter.class);
-    return http.build();
-  }
+    @Bean
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity http, HeaderAuthenticationFilter headerFilter) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(
+                        auth -> auth.requestMatchers(
+                                "/actuator/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/v3/api-docs/**",
+                                "/v3/api-docs.yaml",
+                                "/api/v1/auth/login",
+                                "/api/v1/users/customer")
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated())
+                .addFilterBefore(headerFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
 }
